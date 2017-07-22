60ce4e6dc3ce9956e4aba9311cf408fe342bb81f
Nicolas Carraggi <nicolascarraggi@Nicolas-Carraggis-MacBook-Pro.local>
2017-07-22 23:42:24 +0200

    Only SONY alarm & incoming call working!

--- app/src/main/java/com/example/nicolascarraggi/trgrd/rulesys/devices/AndroidPhone.java
+++ app/src/main/java/com/example/nicolascarraggi/trgrd/rulesys/devices/AndroidPhone.java
@@ -11,6 +11,7 @@ import android.graphics.Color;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.TaskStackBuilder;
 import android.telephony.SmsManager;
+import android.telephony.TelephonyManager;
 import android.util.Log;
 import android.widget.Toast;
 
@@ -40,6 +41,9 @@ public class AndroidPhone extends Device implements NotificationDevice, SendMess
 
     // Constants
 
+    public static final String CALL_PHONE_STATE = "android.intent.action.PHONE_STATE";
+    public static final String CALL_NEW_OUTGOING_CALL = "android.intent.action.NEW_OUTGOING_CALL";
+
     public static final String ALARM_SONY_ALERT_ACTION = "com.sonyericsson.alarm.ALARM_ALERT";
     public static final String ALARM_SONY_SNOOZE_ACTION = "com.sonyericsson.alarm.ALARM_SNOOZE";
     public static final String ALARM_SONY_DISMISS_ACTION = "com.sonyericsson.alarm.ALARM_DISMISS";
@@ -47,10 +51,11 @@ public class AndroidPhone extends Device implements NotificationDevice, SendMess
 
     // com.samsung.sec.android.clockpackage.alarm.
 
-    public static final String ALARM_SAMSUNG_ALERT_ACTION = "com.samsung.sec.android.clockpackage.alarm.ALARM_ALERT";
-    public static final String ALARM_SAMSUNG_SNOOZE_ACTION = "com.samsung.sec.android.clockpackage.alarm.ALARM_SNOOZE";
-    public static final String ALARM_SAMSUNG_DISMISS_ACTION = "com.samsung.sec.android.clockpackage.alarm.ALARM_DISMISS";
-    public static final String ALARM_SAMSUNG_DONE_ACTION = "com.samsung.sec.android.clockpackage.alarm.ALARM_DONE";
+    public static final String ALARM_SAMSUNG_ALERT_ACTION = "com.samsung.sec.android.clockpackage.alarm.ALARM_ALERT"; // WORKS
+    public static final String ALARM_SAMSUNG_SNOOZE_ACTION = "com.samsung.sec.android.clockpackage.alarm.ALARM_SNOOZE"; // DOESN'T WORK ...
+    public static final String ALARM_SAMSUNG_DISMISS_ACTION = "com.samsung.sec.android.clockpackage.alarm.ALARM_DISMISS"; // DOESN'T WORK ...
+    public static final String ALARM_SAMSUNG_DONE_ACTION = "com.samsung.sec.android.clockpackage.alarm.ALARM_DONE"; // DOESN'T WORK ...
+    public static final String ALARM_SAMSUNG_CLOCKPACKAGE = "com.sec.android.app.clockpackage.ClockPackage"; // DOESN'T WORK ...
 
     public static final String ALARM_ALERT_ACTION = "com.android.deskclock.ALARM_ALERT";
     public static final String ALARM_SNOOZE_ACTION = "com.android.deskclock.ALARM_SNOOZE";
@@ -58,6 +63,8 @@ public class AndroidPhone extends Device implements NotificationDevice, SendMess
     public static final String ALARM_DONE_ACTION = "com.android.deskclock.ALARM_DONE";
 
     public static final String SMS_RECEIVED_ACTION = "android.provider.Telephony.SMS_RECEIVED"; // WORKS on emulator & sony phone ( if permission in settings ) !!!
+
+    private int state = 0;
 
     // Android Phone Broadcast Receiver
 
@@ -67,29 +74,51 @@ public class AndroidPhone extends Device implements NotificationDevice, SendMess
             String action = intent.getAction();
             Log.d("TRGRD","AndroidPhone mReceiver "+action);
 
-            if(action.equals(ALARM_SONY_ALERT_ACTION) || action.equals(ALARM_SAMSUNG_ALERT_ACTION) || action.equals(ALARM_ALERT_ACTION)){
+            if(action.equals(ALARM_SONY_ALERT_ACTION)){
                 evAlarmStart();
-            } else if(action.equals(ALARM_SONY_SNOOZE_ACTION) || action.equals(ALARM_SAMSUNG_SNOOZE_ACTION) || action.equals(ALARM_SNOOZE_ACTION)){
+            } else if(action.equals(ALARM_SONY_SNOOZE_ACTION)){
                 evAlarmSnooze();
-            } else if(action.equals(ALARM_SONY_DISMISS_ACTION) || action.equals(ALARM_SAMSUNG_DISMISS_ACTION) || action.equals(ALARM_DISMISS_ACTION)){
+            } else if(action.equals(ALARM_SONY_DISMISS_ACTION)){
                 evAlarmDismiss();
-            } else if(action.equals(ALARM_SONY_DONE_ACTION) || action.equals(ALARM_SAMSUNG_DONE_ACTION) || action.equals(ALARM_DONE_ACTION)){
+            } else if(action.equals(ALARM_SONY_DONE_ACTION)){ // || action.equals(ALARM_SAMSUNG_DONE_ACTION) || action.equals(ALARM_DONE_ACTION)
                 evAlarmDone();
+            } else if(action.equals(CALL_PHONE_STATE)){
+                String stateStr = intent.getExtras().getString(TelephonyManager.EXTRA_STATE);
+                String number = intent.getExtras().getString(TelephonyManager.EXTRA_INCOMING_NUMBER);
+                if(stateStr.equals(TelephonyManager.EXTRA_STATE_IDLE)){
+                    state = TelephonyManager.CALL_STATE_IDLE;
+                    evCallIncStop();
+                }
+                else if(stateStr.equals(TelephonyManager.EXTRA_STATE_OFFHOOK)){
+                    state = TelephonyManager.CALL_STATE_OFFHOOK;
+                    // event on call incoming answered?
+                }
+                else if(stateStr.equals(TelephonyManager.EXTRA_STATE_RINGING)){
+                    state = TelephonyManager.CALL_STATE_RINGING;
+                    callerPhoneNumber = number;
+                    evCallIncStart();
+                }
+                MyLogger.debugLog("TRGRD","AndroidPhone INCOMING CALL from: "+number+", state = "+state);
             }
         }
     };
 
     // Events, states & actions
 
-    private Event mEvAlarmStart, mEvAlarmSnooze, mEvAlarmDismiss, mEvAlarmDone, mEvCallIncStart, mEvCallIncStop;
-    private State mStAlarmGoing, mStCallIncGoing;
+    private Event mEvAlarmStart, mEvAlarmSnooze, mEvAlarmDismiss, mEvAlarmDone, mEvCallIncStart,
+                    mEvCallIncStop, mEvCallIncFavStart, mEvCallIncFavStop, mEvCallIncNoFavStart,
+                    mEvCallIncNoFavStop;
+    private State mStAlarmGoing, mStCallIncGoing, mStCallIncFavGoing, mStCallIncNoFavGoing;
     private Action mAcAlarmDismiss, mAcAlarmSnooze;
     private NotificationAction mAcNotify;
     private SendMessageAction mAcSendMessage;
     private SendMessageCallerAction mAcSendMessageCaller;
 
+    private String callerPhoneNumber = "0486371224";
+
     public AndroidPhone(RuleSystemService ruleSystemService, DeviceManager deviceManager) {
         super(ruleSystemService.getNewId(), "Phone", "Google", "Android", R.drawable.ic_phone_android_black_24dp, ruleSystemService,deviceManager);
+        // Types
         this.eventTypes.put(deviceManager.getEvAlarmAlert().getId(),deviceManager.getEvAlarmAlert());
         this.eventTypes.put(deviceManager.getEvAlarmSnooze().getId(),deviceManager.getEvAlarmSnooze());
         this.eventTypes.put(deviceManager.getEvAlarmDismiss().getId(),deviceManager.getEvAlarmDismiss());
@@ -100,14 +129,23 @@ public class AndroidPhone extends Device implements NotificationDevice, SendMess
         this.actionTypes.put(deviceManager.getAcAlarmSnooze().getId(),deviceManager.getAcAlarmSnooze());
         this.actionTypes.put(deviceManager.getAcAlarmDismiss().getId(),deviceManager.getAcAlarmDismiss());
         this.actionTypes.put(deviceManager.acSendMessage.getId(),deviceManager.acSendMessage);
+        // Events
         this.mEvAlarmStart = new Event(deviceManager.getNewId(),"Phone alarm starts ringing", R.drawable.ic_alarm_black_24dp, this, deviceManager.getEvAlarmAlert(), ruleSystemService.getRuleManager().getRuleEngine());
         this.mEvAlarmSnooze = new Event(deviceManager.getNewId(),"Phone alarm snoozed", R.drawable.ic_alarm_off_black_24dp, this, deviceManager.getEvAlarmSnooze(), ruleSystemService.getRuleManager().getRuleEngine());
         this.mEvAlarmDismiss = new Event(deviceManager.getNewId(),"Phone alarm dismissed", R.drawable.ic_alarm_off_black_24dp, this, deviceManager.getEvAlarmDismiss(), ruleSystemService.getRuleManager().getRuleEngine());
         this.mEvAlarmDone = new Event(deviceManager.getNewId(),"Phone alarm done", R.drawable.ic_alarm_off_black_24dp, this, deviceManager.getEvAlarmDone(), ruleSystemService.getRuleManager().getRuleEngine());
         this.mEvCallIncStart = new Event(deviceManager.getNewId(),"Phone call incoming starts", R.drawable.ic_call_black_24dp, this, deviceManager.getEvCallInc(), ruleSystemService.getRuleManager().getRuleEngine());
         this.mEvCallIncStop = new Event(deviceManager.getNewId(),"Phone call incoming stops", R.drawable.ic_call_end_black_24dp, this, deviceManager.getEvCallInc(), ruleSystemService.getRuleManager().getRuleEngine());
+        this.mEvCallIncFavStart = new Event(deviceManager.getNewId(),"Phone call incoming from FAVORITES starts", R.drawable.ic_call_black_24dp, this, deviceManager.getEvCallInc(), ruleSystemService.getRuleManager().getRuleEngine());
+        this.mEvCallIncFavStop = new Event(deviceManager.getNewId(),"Phone call incoming from FAVORITES stops", R.drawable.ic_call_end_black_24dp, this, deviceManager.getEvCallInc(), ruleSystemService.getRuleManager().getRuleEngine());
+        this.mEvCallIncNoFavStart = new Event(deviceManager.getNewId(),"Phone call incoming from NON FAVORITES starts", R.drawable.ic_call_black_24dp, this, deviceManager.getEvCallInc(), ruleSystemService.getRuleManager().getRuleEngine());
+        this.mEvCallIncNoFavStop = new Event(deviceManager.getNewId(),"Phone call incoming from NON FAVORITES stops", R.drawable.ic_call_end_black_24dp, this, deviceManager.getEvCallInc(), ruleSystemService.getRuleManager().getRuleEngine());
+        // States
         this.mStAlarmGoing = new State(deviceManager.getNewId(),"Phone alarm is ringing", R.drawable.ic_alarm_black_24dp, this, deviceManager.getStAlarmGoing(), false, ruleSystemService.getRuleManager().getRuleEngine());
         this.mStCallIncGoing = new State(deviceManager.getNewId(),"Phone call incoming going", R.drawable.ic_call_black_24dp, this, deviceManager.getStCallIncGoing(), false, ruleSystemService.getRuleManager().getRuleEngine());
+        this.mStCallIncFavGoing = new State(deviceManager.getNewId(),"Phone call incoming from FAVORITES going", R.drawable.ic_call_black_24dp, this, deviceManager.getStCallIncGoing(), false, ruleSystemService.getRuleManager().getRuleEngine());
+        this.mStCallIncNoFavGoing = new State(deviceManager.getNewId(),"Phone call incoming from NON FAVORITES going", R.drawable.ic_call_black_24dp, this, deviceManager.getStCallIncGoing(), false, ruleSystemService.getRuleManager().getRuleEngine());
+        // Actions
         this.mAcAlarmDismiss = new Action(deviceManager.getNewId(),"Phone alarm dismiss", R.drawable.ic_alarm_off_black_24dp, this, deviceManager.getAcAlarmDismiss(), new Callable<String>() {
             @Override
             public String call() throws Exception {
@@ -133,8 +171,14 @@ public class AndroidPhone extends Device implements NotificationDevice, SendMess
         this.events.put(mEvAlarmDone.getId(),mEvAlarmDone);
         this.events.put(mEvCallIncStart.getId(),mEvCallIncStart);
         this.events.put(mEvCallIncStop.getId(),mEvCallIncStop);
+        this.events.put(mEvCallIncFavStart.getId(),mEvCallIncFavStart);
+        this.events.put(mEvCallIncFavStop.getId(),mEvCallIncFavStop);
+        this.events.put(mEvCallIncNoFavStart.getId(),mEvCallIncNoFavStart);
+        this.events.put(mEvCallIncNoFavStop.getId(),mEvCallIncNoFavStop);
         this.states.put(mStAlarmGoing.getId(),mStAlarmGoing);
         this.states.put(mStCallIncGoing.getId(),mStCallIncGoing);
+        this.states.put(mStCallIncFavGoing.getId(),mStCallIncFavGoing);
+        this.states.put(mStCallIncNoFavGoing.getId(),mStCallIncNoFavGoing);
         this.actions.put(mAcAlarmDismiss.getId(),mAcAlarmDismiss);
         this.actions.put(mAcAlarmSnooze.getId(),mAcAlarmSnooze);
         this.actions.put(mAcNotify.getId(),mAcNotify);
@@ -222,10 +266,32 @@ public class AndroidPhone extends Device implements NotificationDevice, SendMess
 
     // Real Events and Actions
 
+    public void evCallIncStart(){
+        System.out.println("[Phone Event] Call incoming start!");
+        mEvCallIncStart.trigger();
+        mStCallIncGoing.setState(true);
+        // Simulation!
+        mEvCallIncFavStart.trigger();
+        mStCallIncFavGoing.setState(true);
+        mEvCallIncNoFavStart.trigger();
+        mStCallIncNoFavGoing.setState(true);
+    }
+
+    public void evCallIncStop(){
+        System.out.println("[Phone Event] Call incoming stop!");
+        mEvCallIncStop.trigger();
+        mStCallIncGoing.setState(false);
+        // Simulation!
+        mEvCallIncFavStop.trigger();
+        mStCallIncFavGoing.setState(false);
+        mEvCallIncNoFavStop.trigger();
+        mStCallIncNoFavGoing.setState(false);
+    }
+
     public void evAlarmStart(){
         System.out.println("[Phone Event] Alarm starts going off!");
         mEvAlarmStart.trigger();
-        mStAlarmGoing.trigger();
+        mStAlarmGoing.setState(true);
     }
 
     public void evAlarmSnooze(){
@@ -247,10 +313,10 @@ public class AndroidPhone extends Device implements NotificationDevice, SendMess
     }
 
     public void acAlarmSnooze(){
-        //Intent sonyIntent = new Intent(ALARM_SONY_SNOOZE_ACTION);
-        //ruleSystemService.getApplicationContext().sendBroadcast(sonyIntent);
-        Intent samsungIntent = new Intent(ALARM_SAMSUNG_SNOOZE_ACTION);
-        ruleSystemService.getApplicationContext().sendBroadcast(samsungIntent);
+        Intent sonyIntent = new Intent(ALARM_SONY_SNOOZE_ACTION);
+        ruleSystemService.getApplicationContext().sendBroadcast(sonyIntent);
+        //Intent samsungIntent = new Intent(ALARM_SAMSUNG_SNOOZE_ACTION);
+        //ruleSystemService.getApplicationContext().sendBroadcast(samsungIntent);
         //Intent stockIntent = new Intent(ALARM_SNOOZE_ACTION);
         //ruleSystemService.getApplicationContext().sendBroadcast(stockIntent);
         Toast.makeText(ruleSystemService, "Alarm snoozed by TRGRD", Toast.LENGTH_SHORT).show();
@@ -258,10 +324,10 @@ public class AndroidPhone extends Device implements NotificationDevice, SendMess
     }
 
     public void acAlarmDismiss(){
-        //Intent sonyIntent = new Intent(ALARM_SONY_DISMISS_ACTION);
-        //ruleSystemService.getApplicationContext().sendBroadcast(sonyIntent);
-        Intent samsungIntent = new Intent(ALARM_SAMSUNG_DISMISS_ACTION);
-        ruleSystemService.getApplicationContext().sendBroadcast(samsungIntent);
+        Intent sonyIntent = new Intent(ALARM_SONY_DISMISS_ACTION);
+        ruleSystemService.getApplicationContext().sendBroadcast(sonyIntent);
+        //Intent samsungIntent = new Intent(ALARM_SAMSUNG_DISMISS_ACTION);
+        //ruleSystemService.getApplicationContext().sendBroadcast(samsungIntent);
         //Intent stockIntent = new Intent(ALARM_DISMISS_ACTION);
         //ruleSystemService.getApplicationContext().sendBroadcast(stockIntent);
         Toast.makeText(ruleSystemService, "Alarm dismissed by TRGRD", Toast.LENGTH_SHORT).show();
@@ -360,6 +426,10 @@ public class AndroidPhone extends Device implements NotificationDevice, SendMess
     @Override
     public void acSendMessageCaller(String message) {
         MyLogger.debugLog("TRGRD","AndroidPhone acSendMessage to ... : "+message);
+        SmsManager smsManager =     SmsManager.getDefault();
+        // caller phone number simulation !!
+        smsManager.sendTextMessage(callerPhoneNumber, null, message, null, null);
+        acNotify("Phone","Your message has been sent to the caller!", NotificationAction.NotificationActionType.NONE);
     }
 
     @Override
@@ -392,6 +462,7 @@ public class AndroidPhone extends Device implements NotificationDevice, SendMess
         MyLogger.debugLog("TRGRD","AndroidPhone acSendMessage to "+phonenumber+": "+message);
         SmsManager smsManager =     SmsManager.getDefault();
         smsManager.sendTextMessage(phonenumber, null, message, null, null);
+        acNotify("Phone","Your message has been sent to "+phonenumber+"!", NotificationAction.NotificationActionType.NONE);
     }
     
     // Register & UnRegister Android Phone Broadcast Receiver
@@ -402,16 +473,19 @@ public class AndroidPhone extends Device implements NotificationDevice, SendMess
         filter.addAction(ALARM_SONY_SNOOZE_ACTION);
         filter.addAction(ALARM_SONY_DISMISS_ACTION);
         filter.addAction(ALARM_SONY_DONE_ACTION);
-        // SAMSUNG
-        filter.addAction(ALARM_SAMSUNG_ALERT_ACTION);
-        filter.addAction(ALARM_SAMSUNG_SNOOZE_ACTION);
-        filter.addAction(ALARM_SAMSUNG_DISMISS_ACTION);
-        filter.addAction(ALARM_SAMSUNG_DONE_ACTION);
-        // STOCK
-        filter.addAction(ALARM_ALERT_ACTION);
-        filter.addAction(ALARM_SNOOZE_ACTION);
-        filter.addAction(ALARM_DISMISS_ACTION);
-        filter.addAction(ALARM_DONE_ACTION);
+//        // SAMSUNG
+//        filter.addAction(ALARM_SAMSUNG_ALERT_ACTION);
+//        filter.addAction(ALARM_SAMSUNG_SNOOZE_ACTION);
+//        filter.addAction(ALARM_SAMSUNG_DISMISS_ACTION);
+//        filter.addAction(ALARM_SAMSUNG_DONE_ACTION);
+//        filter.addAction(ALARM_SAMSUNG_CLOCKPACKAGE);
+//        // STOCK
+//        filter.addAction(ALARM_ALERT_ACTION);
+//        filter.addAction(ALARM_SNOOZE_ACTION);
+//        filter.addAction(ALARM_DISMISS_ACTION);
+//        filter.addAction(ALARM_DONE_ACTION);
+        filter.addAction(CALL_PHONE_STATE);
+        filter.addAction(CALL_NEW_OUTGOING_CALL);
         ruleSystemService.registerReceiver(mReceiver, filter);
         Log.d("TRGRD","AndroidPhone mReceiver registered!");
     }
